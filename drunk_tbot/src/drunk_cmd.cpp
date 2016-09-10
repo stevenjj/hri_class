4c662ce7398e03d81f39e8a3751a46d7798dd749
Steven Jorgensen <stevenjensj@gmai.com>
2016-09-10 11:59:37 -0700

    basic forward and angular commands

--- drunk_tbot/src/drunk_cmd.cpp
+++ drunk_tbot/src/drunk_cmd.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
 #include "turtlesim/Color.h"
 #include "turtlesim/Pose.h"
 
@@ -6,22 +7,46 @@ class TurtleCmdNode{
 public:
   ros::NodeHandle node;
   ros::Subscriber pose_sub;
-  ros::Publisher  cmd_pub;  
+  ros::Publisher  cmd_pub;   
+
+  void cmd_callback(const turtlesim::PoseConstPtr& msg);
+  geometry_msgs::Twist get_cmd();
 
   TurtleCmdNode();
   ~TurtleCmdNode();  
 };
 
+TurtleCmdNode::TurtleCmdNode(){} // empty constructor
+TurtleCmdNode::~TurtleCmdNode(){} // standard destructor
 
-void chatterCallback(const turtlesim::Pose& msg)
-{
-  ROS_INFO("I heard: x [%f]", msg.x);
+
+geometry_msgs::Twist TurtleCmdNode::get_cmd(){
+  geometry_msgs::Twist vel_msg;
+  vel_msg.linear.x = 0.1; //(double)(rand() % 10 +1)/4.0;
+  vel_msg.linear.y = 0.1;
+  vel_msg.linear.z =0;
+  //set a random angular velocity in the y-axis
+  vel_msg.angular.x = 0;
+  vel_msg.angular.y = 0;
+  vel_msg.angular.z = 0.1;  
+  return vel_msg;
+
 }
+
+
+void TurtleCmdNode::cmd_callback(const turtlesim::PoseConstPtr& msg){
+  ROS_INFO("I heard: x [%f]", msg->x);
+  cmd_pub.publish( get_cmd() );
+}
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "drunk_cmd");
-  ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/turtle1/pose", 1000, chatterCallback);
+  TurtleCmdNode turtle;
+  turtle.pose_sub = turtle.node.subscribe<turtlesim::Pose>("/turtle1/pose", 1000, boost::bind(&TurtleCmdNode::cmd_callback, &turtle, _1) );
+//  turtle.pose_sub = turtle.node.subscribe("/turtle1/pose", 1000, TurtleCmdNode::cmd_callback);  
+  turtle.cmd_pub = turtle.node.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);  
+
   ros::spin();
 }
