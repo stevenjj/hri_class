aa587e08b0285d7e7b6a1a26ac9cf2e0525147bd
Steven Jorgensen <stevenjensj@gmai.com>
2016-09-11 01:35:13 -0700

    continuous back and forth between two desired goals with different pen colors

--- drunk_tbot/include/drunk_cmd.h
+++ drunk_tbot/include/drunk_cmd.h
@@ -15,6 +15,8 @@
 #define STATE_MOVE_STRAIGHT 1
 #define STATE_TURN_TO_GOAL 2
 
+#define STATE_MOVE_TO_GOAL 3
+
 
 class TurtleCmdNode{
 public:
@@ -32,33 +34,83 @@ public:
   int current_state;
   int current_task;
 
-  float kp;
-  float ki;    
+  std::string current_task_description;
+
+  float kp_head;
+  float ki_head;  
+
+  float kp_lin;
+  float kd_lin;
+  float ki_lin;    
   float error_cum;
 
+  float MAX_LINEAR_VEL;
+  float MAX_ANGULAR_VEL;  
 
   void cmd_callback(const turtlesim::PoseConstPtr& msg);
   void update_current_pose(const turtlesim::PoseConstPtr& msg);
+  void update_state(int new_state);
+  void do_next_task();  
 
   void change_pen(int r, int g, int b, int width, int off);
 
-  void set_goal(float goal_x, float goal_y, float goal_theta_deg);
+  void set_goal(float goal_x, float goal_y);
   void update_task();
 
   geometry_msgs::Twist get_cmd(const turtlesim::PoseConstPtr& msg);
+
+  float max_linear_command(float linear_command);
+  float max_heading_command(float heading_command);  
 
 
   TurtleCmdNode();
   ~TurtleCmdNode();  
 };
 
-TurtleCmdNode::TurtleCmdNode(): kp(1), ki(1), error_cum(0) {
+TurtleCmdNode::TurtleCmdNode(): kp_head(2), ki_head(0.01), kp_lin(2), kd_lin(0.05), ki_lin(0.0), error_cum(0),
+                                current_task(-1), MAX_LINEAR_VEL(10), MAX_ANGULAR_VEL(3) {
   states.push_back(STATE_IDLE);
   states.push_back(STATE_MOVE_STRAIGHT);
   states.push_back(STATE_TURN_TO_GOAL);
   current_state = STATE_IDLE;
+
 } //Initialize gains and states 
 
 TurtleCmdNode::~TurtleCmdNode(){} // standard destructor
+
+// Simple utils
+float rad_2_deg(float rads){
+  return (180.0/PI)*rads;
+}
+
+float deg_2_rad(float degs){
+  return (PI/180.0)*degs;
+}
+
+float sign(float num){
+  if (num > 0){
+    return 1;
+  }else if (num < 0){
+    return -1;
+  }else{
+    return 0;
+  }
+}
+
+float TurtleCmdNode::max_linear_command(float linear_command){
+  if (fabs(linear_command) > MAX_LINEAR_VEL){
+    return MAX_LINEAR_VEL;
+  }else{
+    return fabs(linear_command);
+  }
+}
+
+float TurtleCmdNode::max_heading_command(float heading_command){
+  if (fabs(heading_command) > MAX_ANGULAR_VEL){
+    return MAX_ANGULAR_VEL;
+  }else{
+    return fabs(heading_command);
+  }
+}
 
 #endif
--- drunk_tbot/src/drunk_cmd.cpp
+++ drunk_tbot/src/drunk_cmd.cpp
@@ -2,24 +2,26 @@
 
 // State machine:
 //  turn to goal -> move_straight -> idle
-
-float rad_2_deg(float rads){
-  return (180.0/PI)*rads;
-}
-
-float deg_2_rad(float degs){
-  return (PI/180.0)*degs;
-}
+//  @ Idle update task
+//  if task is empty, do nothing.
 
 void TurtleCmdNode::update_current_pose(const turtlesim::PoseConstPtr& msg){
-    current_pose = *msg;
+  current_pose = *msg;
     //ROS_INFO("current: x [%f]", msg->x);
 }
+void TurtleCmdNode::update_state(int new_state){
+  current_state = new_state;
+}
+
+void TurtleCmdNode::do_next_task(){
+  current_task += 1;
+}
+
 void TurtleCmdNode::change_pen(int r, int g, int b, int width, int off){
   pen_srv.request.r = r;
   pen_srv.request.g = g;
   pen_srv.request.b = b;
-  pen_srv.request.width = r;
+  pen_srv.request.width = width;
   pen_srv.request.off = off;
 
   if (pencolor_client.call(pen_srv))
@@ -27,11 +29,16 @@ void TurtleCmdNode::change_pen(int r, int g, int b, int width, int off){
     ROS_INFO("Color Change");
   }
 }
-void TurtleCmdNode::set_goal(float goal_x, float goal_y, float goal_theta_deg){
+
+void TurtleCmdNode::set_goal(float goal_x, float goal_y){
   goal_pose.x = goal_x;
-  goal_pose.x = goal_y;  
-  goal_pose.theta = deg_2_rad(goal_theta_deg);
-  current_state = STATE_TURN_TO_GOAL;
+  goal_pose.y = goal_y;  
+
+//  goal_pose.theta = deg_2_rad(goal_theta_deg);
+  goal_pose.theta = atan2( (goal_y - current_pose.y), 
+                           (goal_x - current_pose.x) );
+
+  update_state(STATE_TURN_TO_GOAL);
 }
 
 geometry_msgs::Twist TurtleCmdNode::get_cmd(const turtlesim::PoseConstPtr& msg){
@@ -44,27 +51,50 @@ geometry_msgs::Twist TurtleCmdNode::get_cmd(const turtlesim::PoseConstPtr& msg){
   // STATE: Turn to Goal
   if (current_state == STATE_TURN_TO_GOAL){
     float heading_error = goal_pose.theta - current_pose.theta;
-    if (fabs(heading_error) < 0.05){
+    ROS_INFO("heading_error: %f", heading_error);
+    if (fabs(heading_error) < 0.01){
       heading_command = 0;
+      update_state(STATE_MOVE_STRAIGHT);
     }else{
-      heading_command = kp*heading_error;
+      heading_command = kp_head*heading_error;
     }
 
   // STATE: MOVE STRAIGHT
   }else if(current_state == STATE_MOVE_STRAIGHT){
-    vel_msg.angular.z = 0;
+      // Calculate errors
+      float error_x = (goal_pose.x - current_pose.x);
+      float error_y = (goal_pose.y - current_pose.y);
+      float error_mag = sqrt( pow(error_x,2) + pow(error_y, 2) );
 
-  // STATE: MUST BE IDLE    
+      float goal_dir_x = cos(goal_pose.theta);
+      float goal_dir_y = sin(goal_pose.theta);  
+      
+      // Dot product of heading and e_pos = [dx, dy]^T
+      float error_sign = sign((goal_dir_x*error_x) + (goal_dir_y*error_y)); 
+      error_cum += (error_sign*error_mag);      
+
+      // Calculate Control Command
+      linear_command = kp_lin*error_sign*error_mag - kd_lin*current_pose.linear_velocity + ki_lin*error_cum;
+
+      //ROS_INFO("goal_x: %f, current_x: %f, goal_y: %f, current_y: %f", goal_pose.x, current_pose.x, goal_pose.y, current_pose.y);
+      ROS_INFO("mag: %f", error_mag);
+
+      if (error_mag < 0.12){
+        error_cum = 0;
+        update_state(STATE_IDLE);
+      }
+
+  // STATE: IDLE    
   }else{
+    ROS_INFO("IDLE");
     linear_command = 0;
     heading_command = 0;    
   }
 
-
   // Finalize command
-  vel_msg.linear.x = linear_command;//(double)(rand() % 10 +1)/4.0;
-  vel_msg.angular.z = heading_command; 
-  ROS_INFO("linear x: %f, angular z: %f", vel_msg.linear.x, vel_msg.angular.z);
+  vel_msg.linear.x = max_linear_command(linear_command)*sign(linear_command);//(double)(rand() % 10 +1)/4.0;
+  vel_msg.angular.z = max_heading_command(heading_command)*sign(heading_command); 
+  //ROS_INFO("linear x: %f, angular z: %f", vel_msg.linear.x, vel_msg.angular.z);
   return vel_msg;
 }
 
@@ -73,69 +103,34 @@ void TurtleCmdNode::cmd_callback(const turtlesim::PoseConstPtr& msg){
   // Update state here
   update_current_pose(msg);
 
+  // Main State Machine Logic
+  if ((current_state == STATE_TURN_TO_GOAL) || (current_state == STATE_MOVE_STRAIGHT)){
+    cmd_pub.publish( get_cmd(msg) );    
+  }else if(current_state == STATE_IDLE){
+    do_next_task();
+    update_task();    
+  }
 
-  update_task();
-
-  ROS_INFO("current_state: %i", states[0]);
-  cmd_pub.publish( get_cmd(msg) );
 }
 
 void TurtleCmdNode::update_task(){
   if (current_task == 0){
-    change_pen(0, 255, 0, 0.01, 0);
-    set_goal(current_pose.x, current_pose.y, 0);
+    current_task_description = "Go to starting position";
+    change_pen(0, 255, 0, 3, 0);
+    set_goal(1, 9);
+//    set_goal(current_pose.x, current_pose.y, 0);
   }else if(current_task == 1){
-
+    change_pen(255, 0, 0, 3, 0);
+    set_goal(9,1);
+    current_task = -1;
   }else if(current_task == 2){
 
   }else{
+    ROS_INFO("Finished all tasks");
 
   }
 
 }
-/*
-// States:
-//    In Motion
-//      move forward
-//      turn x degrees
-//    Idle
-
-goal_x = ;
-goal_y = ;
-goal_heading = ;
-heading_goal;
-if state is in motion = [move or turn]:
-  if state is move forward:
-    continue moving forward
-  elif state is turn heading:
-    continue turning
-elif state is idle:
-  move_to_next_task
-  set_new_goals and change goal_x, goal_y, goal_heading
-  change pen color
-
-// Tasks:
-// Draw U
-// Move to starting position
-// Draw T
-
-if state == idle {
-  move to next goal  
-}
-
-x_start = x_o
-calculate delta_x:
-heading: x_hat = cos(th)*i + sin(th)*j
-x_goal = x_hat*number of steps
-
-error = ||x_goal - x_start||_2 *  sign(heading_dir dot ||x_goal-x_start||_2
-
-vel = error*kp
-
-
-
-*/
-
 
 
 
