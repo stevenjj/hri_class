18792362847512780e20638aded817591daed3ed
Steven Jorgensen <stevenjensj@gmail.com>
2016-10-19 13:19:31 -0700

    created turtlenode class for speech tbot. moves if speech commands are available

--- speech_controlled_tbot/src/speech_tbot.cpp
+++ speech_controlled_tbot/src/speech_tbot.cpp
@@ -5,6 +5,54 @@
 #include "turtlesim/SetPen.h"
 #include "hlpr_speech_msgs/StampedString.h"
 
+#define FWD_VEL 1
+#define TURN_VEL 0.5
+
+class TurtleNode{
+public:
+  ros::NodeHandle node;
+  ros::Subscriber speech_sub;
+  ros::Publisher  cmd_pub;
+
+  double forward_vel;
+  double turn_vel;  
+
+  void speech_callback(const hlpr_speech_msgs::StampedStringConstPtr &msg);
+
+  TurtleNode();
+  ~TurtleNode();
+};
+TurtleNode::TurtleNode(){
+	forward_vel = FWD_VEL;
+	turn_vel = TURN_VEL;	
+}
+TurtleNode::~TurtleNode(){}
+
+void TurtleNode::speech_callback(const hlpr_speech_msgs::StampedStringConstPtr &msg){
+	geometry_msgs::Twist vel_msg;
+
+	std::string forward_command("FORWARD");
+	std::string turn_left_command("LEFT");
+	std::string turn_right_command("RIGHT");		
+
+	if (msg->keyphrase.compare(forward_command) == 0){
+		ROS_INFO("The command is to move forward");
+		vel_msg.linear.x = forward_vel;
+		cmd_pub.publish(vel_msg);
+	}
+	if (msg->keyphrase.compare(turn_left_command) == 0){
+		ROS_INFO("The command is to turn left");
+		vel_msg.angular.z = turn_vel;
+		cmd_pub.publish(vel_msg);		
+	}
+	if (msg->keyphrase.compare(turn_right_command) == 0){
+		ROS_INFO("The command is to turn right");
+		vel_msg.angular.z = -turn_vel;
+		cmd_pub.publish(vel_msg);		
+	}
+}
+
+
 void callback(const hlpr_speech_msgs::StampedStringConstPtr &msg){
 	//std::cout << msg->keyphrase << std::endl;
 
@@ -26,11 +74,13 @@ void callback(const hlpr_speech_msgs::StampedStringConstPtr &msg){
 
 int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "speech_tbot");
+  //ros::NodeHandle n;
+  //ros::Subscriber sub = n.subscribe<hlpr_speech_msgs::StampedString>("/hlpr_speech_commands", 1, callback);
 
-  ros::init(argc, argv, "social_tbot");
-  ros::NodeHandle n;
-
-  ros::Subscriber sub = n.subscribe<hlpr_speech_msgs::StampedString>("/hlpr_speech_commands", 1, callback);
+  TurtleNode turtle;
+  turtle.speech_sub = turtle.node.subscribe<hlpr_speech_msgs::StampedString>("/hlpr_speech_commands", 1, boost::bind(&TurtleNode::speech_callback, &turtle, _1));
+  turtle.cmd_pub = turtle.node.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);  
 
   ros::spin();
 
